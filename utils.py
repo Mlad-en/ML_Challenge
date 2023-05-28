700cb1ebc90091673ed9fdeb5ff620df3236293c
Mladen Mladenov <mladen.mladenov@tumba.solutions>
2023-05-28 13:54:55 +0200

    adding logistic regression models.

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 from typing import Any
 
+from imblearn.over_sampling import SMOTENC
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import cross_validate, GridSearchCV, train_test_split
+from sklearn.model_selection import (
+    cross_validate, GridSearchCV, train_test_split, RandomizedSearchCV
+)
 
-from constants import Columns, ModelConstants
+from sklearn.preprocessing import LabelEncoder
+
+from constants import Columns, ModelConstants, Resample
 
 DATA_TYPES = {
     Columns.TRANSACTION: np.int8,
@@ -37,9 +42,32 @@ class DataSplit:
     Represents a split of data into predictors and outcomes.
     """
 
-    def __init__(self, x, y):
-        self.predictors = x
-        self.outcome = y
+    def __init__(self, x, y, resample: Resample):
+        if resample == Resample.no_resample:
+            self.predictors = x
+            self.outcome = y
+
+        elif resample == Resample.smote:
+
+            cols = list(x.dtypes == "category")
+            cat_col_ind = [ind for ind, val in enumerate(cols) if val]
+
+            x[Columns.CUSTOMER_TYPE] = x[Columns.CUSTOMER_TYPE].astype("object")
+            x[Columns.SPECIFIC_HOLIDAY] = x[Columns.SPECIFIC_HOLIDAY].astype("object")
+
+            x_res, y_res = self._resample_smote(x, y, cat_col_ind)
+            self.predictors = x_res
+            self.outcome = y_res
+
+        else:
+            raise NotImplementedError(f"Resampling Method {resample.value} is not implemented.")
+
+    @staticmethod
+    def _resample_smote(x, y, columns):
+        return SMOTENC(
+            random_state=ModelConstants.RANDOM_STATE,
+            categorical_features=columns
+            ).fit_resample(x, y)
 
 
 class Data:
@@ -47,9 +75,9 @@ class Data:
     Represents training and testing data splits.
     """
 
-    def __init__(self, x_train, x_test, y_train, y_test):
-        self.TRAINING = DataSplit(x_train, y_train)
-        self.TESTING = DataSplit(x_test, y_test)
+    def __init__(self, x_train, x_test, y_train, y_test, sampling):
+        self.TRAINING = DataSplit(x_train, y_train, sampling)
+        self.TESTING = DataSplit(x_test, y_test, Resample.no_resample)
 
 
 class TransactionDataset:
@@ -71,12 +99,13 @@ class TransactionDataset:
         include_cols = [col for col in self.data.columns if not col == Columns.TRANSACTION]
         return self.data.loc[:, include_cols]
 
-    def get_training_test_split(self):
+    def get_training_test_split(self, resample: Resample | None = None):
         """
         Split the dataset into training and testing sets.
 
         :return: Data object containing the training and testing splits.
         """
+
         x_train, x_test, y_train, y_test = train_test_split(
             self._predictors,
             self._target,
@@ -85,7 +114,9 @@ class TransactionDataset:
             random_state=ModelConstants.RANDOM_STATE
         )
 
-        return Data(x_train, x_test, y_train, y_test)
+        sampling = resample if resample else Resample.no_resample
+
+        return Data(x_train, x_test, y_train, y_test, sampling)
 
 
 class TuneHyperParams:
@@ -106,7 +137,7 @@ class TuneHyperParams:
         self.jobs = jobs
         self.scoring = scoring
 
-    def grid_search(self, model, parameters: dict[str, list[Any]]):
+    def full_grid_search(self, model, parameters: dict[str, list[Any]]):
         """
         Perform grid search for hyperparameter tuning.
 
@@ -115,6 +146,24 @@ class TuneHyperParams:
         :return: TuneHyperParams utility object.
         """
         self.gs = GridSearchCV(
+            model,
+            parameters,
+            n_jobs=self.jobs,
+            cv=self.cross_validation,
+            scoring=ModelConstants.F1_SCORE,
+        )
+
+        return self
+    
+    def random_grid_search(self, model, parameters: dict[str, list[Any]]):
+        """
+        Perform grid search for hyperparameter tuning.
+
+        :param model: Model to be tuned.
+        :param parameters: Hyperparameter grid.
+        :return: TuneHyperParams utility object.
+        """
+        self.gs = RandomizedSearchCV(
             model,
             parameters,
             n_jobs=self.jobs,
@@ -141,7 +190,7 @@ class TuneHyperParams:
         :return: Grid search object.
         """
         print(self.gs.best_params_)
-        print(f"Best parameter (CV score{self.gs.best_score_}:.3f):")
+        print(f"Best parameter (CV score: {self.gs.best_score_:.3f}):")
         return self.gs
 
 
